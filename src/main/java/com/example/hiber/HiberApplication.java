5fc9e4f46f464f5cdc188ffeca781aa805afaf61
AZharick <131703047+AZharick@users.noreply.github.com>
2024-03-25 18:05:31 +0300

    jpa init

--- src/main/java/com/example/hiber/HiberApplication.java
+++ src/main/java/com/example/hiber/HiberApplication.java
@@ -1,5 +1,6 @@
 package com.example.hiber;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HiberApplication implements CommandLineRunner {
 
    public static void main(String[] args) {
-      SpringApplication.run(HiberApplication.class, args);
+      SpringApplication app = new SpringApplication(HiberApplication.class);
+      app.setBannerMode(Banner.Mode.OFF);
+      app.run(args);
+
    }
 
    @Override
    public void run(String... args) {
+      System.out.println("\n*** App started ***\n");
    }
 
 }
